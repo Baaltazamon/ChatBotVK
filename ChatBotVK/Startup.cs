703985841db0761138e8ea5bcce78075a63e4649
Baaltazamon <3gpabbiu4eji@mail.ru>
2020-12-06 23:13:13 +0300

    Test

--- ChatBotVK/Startup.cs
+++ ChatBotVK/Startup.cs
@@ -42,17 +42,14 @@ namespace ChatBotVK
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
 
             app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseMvc();
         }
     }
 }
